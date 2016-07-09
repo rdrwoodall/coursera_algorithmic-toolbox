5deeaa9952d141e2657ba60b8760ed3363bb800b
Robert Woodall <rdrwoodall@gmail.com>
2016-07-09 06:22:53 -0600

    Provide efficient solution to gcd and complete lcm implementation.

--- Assignments/Week2/GCD.java
+++ Assignments/Week2/GCD.java
@@ -2,23 +2,32 @@ import java.util.*;
 
 public class GCD {
   private static int gcd(int a, int b) {
-    int current_gcd = 1;
-    for(int d = 2; d <= a && d <= b; ++d) {
-      if (a % d == 0 && b % d == 0) {
-        if (d > current_gcd) {
-          current_gcd = d;
+        int current_gcd = 1;
+        for(int d = 2; d <= a && d <= b; ++d) {
+            if (a % d == 0 && b % d == 0) {
+                if (d > current_gcd) {
+                    current_gcd = d;
+                }
+            }
         }
-      }
+
+        return current_gcd;
     }
 
-    return current_gcd;
-  }
+    private static long gcdEfficient(long num1, long num2) {
+        if (num2 == 0) {
+            return num1;
+        }
 
-  public static void main(String args[]) {
-    Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
+        long remainder = num1 % num2;
+        return gcdEfficient(num2, remainder);
+    }
 
-    System.out.println(gcd(a, b));
-  }
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(gcdEfficient(a, b));
+    }
 }
--- Assignments/Week2/LCM.java
+++ Assignments/Week2/LCM.java
@@ -1,16 +1,32 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class LCM {
-  private static long lcm(int a, int b) {
-    //write your code here
-    return a * b;
-  }
+  private static BigInteger lcm(int a, int b) {
+        //write your code here
+        BigInteger intA = new BigInteger(Integer.toString(a));
+        BigInteger intB = new BigInteger(Integer.toString(b));
 
-  public static void main(String args[]) {
-    Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
+        BigInteger product = intA.multiply(intB);
 
-    System.out.println(lcm(a, b));
-  }
+        long gcd = gcdEfficient(a,b);
+        return product.divide(BigInteger.valueOf(gcd));
+    }
+
+    private static long gcdEfficient(long num1, long num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+
+        long remainder = num1 % num2;
+        return gcdEfficient(num2, remainder);
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(lcm(a, b));
+    }
 }
