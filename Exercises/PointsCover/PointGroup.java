a5472991a82d73068e27aa77d1f593f22983e21e
Robert Woodall <rdrwoodall@gmail.com>
2016-07-12 11:46:20 -0600

    Add problem description to PointsCover problem.

--- Exercises/PointsCover/PointGroup.java
+++ Exercises/PointsCover/PointGroup.java
@@ -1,6 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+
+Problem Description
+
+Let's consider the following situation. You've invited a lot of children to a 
+celebration party, and you want to entertain them and also teach them something 
+in the process. You are going to hire a few teachers and divide the children into 
+groups and assign a teacher to each of the groups this teacher will work with 
+this group through the whole party. 
+
+But you know that for a teacher to work with a group of children efficiently,
+children of that group should be of relatively the same age. More specifically the age 
+of any two children in the same group should differ by at most, one year. 
+
+Also, you want to minimize the number of groups. Because you want to hire fewer teachers, 
+and spend the money on presents and other kinds of entertainment for the children. 
+So, you need to divide children into the minimum possible number of groups. 
+Such that the age of any two children in any group differs by at most one year. 
+
+*/
+
 public class PointGroup {
     // Assumes sorted point list as input
     public static List<List<Double>> groupPoints(double[] points) {
