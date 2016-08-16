11c780e88b3b3a25ec83ecb2a9c66256eec45f7f
Robert Woodall <rdrwoodall@gmail.com>
2016-08-15 19:33:42 -0600

    Implement solution to majority element in a dataset using non-divide and conquer solution.

---
+++ Assignments/Week4/MajorityElement.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class MajorityElement {
+    private static int getMajorityElement(int[] a, int left, int right) {
+        if (left == right) {
+            return -1;
+        }
+        if (left + 1 == right) {
+            return a[left];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // move list to map to keep track of each value in 'a'
+        for (int elementKey : a) {
+            Integer currentVal = map.get(elementKey);
+            map.put(elementKey, currentVal == null ? 1 : currentVal + 1);
+        }
+
+
+        // determine majority value
+        int majorityValue = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (majorityValue != 0 && entry.getValue() > majorityValue) {
+                majorityValue = entry.getValue();
+            } else if (majorityValue == 0){
+                majorityValue = entry.getValue();
+            }
+        }
+
+        boolean isValid = majorityValue > (a.length / 2) ? true : false;
+        return isValid ? majorityValue : -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        if (getMajorityElement(a, 0, a.length) != -1) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
+
